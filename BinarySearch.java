868e15b6a3058635c2a8cee7bba31e44003b5ee1
BharaniReddy02 <bharanichappidi@gmail.com>
2025-06-12 09:53:37 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        Arrays.sort(arr);
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] Array = {2, 10, 14, 4, 3, 12, 6};
+        int target = 10;
+
+        int result = binarySearch(Array, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
